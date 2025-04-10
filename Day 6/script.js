fab24291a18dc7fdd735a7820a8c758a5fcef870
Ajayi Gloria <Adebayoadedoyin076@gmail.com>
2025-04-09 19:04:28 -1100

    initial commit

---
+++ Day 6/script.js
@@ -0,0 +1,35 @@
+document.getElementById('bmiForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+const age = parseInt(document.getElementById('age').value);
+const height = parseFloat(document.getElementById('height').value);
+const weight = parseFloat(document.getElementById('weight').value);
+
+if (isNaN(age) || isNaN(height) || isNaN(weight)) {
+    document.getElementById('result').innerHTML = "Please fill in all fields correctly.";
+    return;
+}
+
+const bmi = weight / (height * height);
+let status = "";
+
+if (bmi < 18.5) {
+    status = "Underweight";
+    color = "blue";
+} else if (bmi >= 18.5 && bmi < 25) {
+    status = "Normal weight";
+    color = "green";
+} else if (bmi >= 25 && bmi < 30) {
+    status = "Overweight";
+    color = "orange";
+} else {
+    status = "Obese";
+    color = "red";
+}
+
+let ageNote = age < 18 ? "Note: BMI is less reliable for people under 18." : "";
+
+
+document.getElementById('result').innerHTML =
+`Your BMI is <strong>${bmi.toFixed(2)}</strong> - <strong>${status}</strong><br>${ageNote}`;
+});
